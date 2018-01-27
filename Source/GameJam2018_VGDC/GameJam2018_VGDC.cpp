5a79ac6e49a6be81133acb0b027fc8807e277380
DJ <dj@schilens.com>
2018-01-26 22:25:18 -0500

    Created UE4 Project
    Featuring a yummy Creative-Commons-Licensed Pizza icon

---
+++ Source/GameJam2018_VGDC/GameJam2018_VGDC.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameJam2018_VGDC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJam2018_VGDC, "GameJam2018_VGDC" );
---
+++ Source/GameJam2018_VGDC/GameJam2018_VGDCGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJam2018_VGDCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM2018_VGDC_API AGameJam2018_VGDCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
