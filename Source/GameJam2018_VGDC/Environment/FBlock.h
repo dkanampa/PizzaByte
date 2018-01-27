19c4844b4c3f4030720790cdee8f36a8a9ff29f1
Joshua Good <jegood@ncsu.edu>
2018-01-27 09:54:42 -0500

    Block and Sector Updates

--- Source/GameJam2018_VGDC/Environment/FBlock.h
+++ Source/GameJam2018_VGDC/Environment/FBlock.h
@@ -14,13 +14,8 @@ public:
 
 	FBlock(FSector* _ParentSector) :ParentSector(_ParentSector) {};
 
-	// I.e. map of whether the building is occupied (by another business) to
-	//   where a new tower mesh can be placed
-	// Stored as such because we cannot access individual mesh instances, nor 
-	//   do we really need to; we just need to know where, if anywhere, we can 
-	//   place a new pizza transmission tower.
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-		TMap<FVector, bool> Buildings;
+	// Pointer means we can have it null indicating no tower present
+	FPizzaNode* OccupyingNode;
 
 	FSector* ParentSector;
 
