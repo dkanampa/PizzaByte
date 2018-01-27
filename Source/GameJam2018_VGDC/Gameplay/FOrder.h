5f18f7d44f6bd062ebd6b1d20c17a35a3382fffe
Joshua Good <jegood@ncsu.edu>
2018-01-27 09:42:43 -0500

    Added PizzaPlayer logic and completed Order members. PursueOrder in progress.

--- Source/GameJam2018_VGDC/Gameplay/FOrder.h
+++ Source/GameJam2018_VGDC/Gameplay/FOrder.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "CoreMinimal.h"
-#include "FOrder.generated.h" 
+#include "FOrder.generated.h"
+#include "Environment/FBlock.h"
 
 USTRUCT(BlueprintType)
 struct FOrder
 {
 	GENERATED_USTRUCT_BODY();
-
+	EPizzaTopping PizzaType;
+	FBlock Block;
+	float TimePlaced;
+	float ExpireTime;
 };
--- Source/GameJam2018_VGDC/Gameplay/PizzaPlayer.h
+++ Source/GameJam2018_VGDC/Gameplay/PizzaPlayer.h
@@ -5,12 +5,22 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "PizzaPlayer.generated.h"
+#include "FPizzaNode.h"
+#include "Environment/FDistrict.h"
+#include "Environment/FSector.h"
+#include "FOrder.h"
 
 UCLASS()
 class GAMEJAM2018_VGDC_API APizzaPlayer : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere);
+	int32 Funds;
+	TArray<FPizzaNode> Nodes;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere);
+	TArray<FDistrict*> PermittedDistricts;
 protected:
 	virtual void BeginPlay() override;
 
@@ -18,7 +28,7 @@ public:
 	APizzaPlayer();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
-	
+	bool PurchaseTowerInSector(FSector* Sector);
+	bool PursueOrder(FOrder& Order);
+	bool hasBoughtSectorPermit(FDistrict* District)
 };
